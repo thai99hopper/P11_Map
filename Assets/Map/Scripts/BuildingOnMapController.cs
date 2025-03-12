a9b28da92c681e9f5436a75c74987bfcb0b4268f
Kietbm <bangminhkiet@hoppergames.io>
2025-03-12 18:01:22 +0700

    no message

--- Assets/Map/Scripts/BuildingOnMapController.cs
+++ Assets/Map/Scripts/BuildingOnMapController.cs
@@ -2,20 +2,4 @@ using UnityEngine;
 
 public partial class BuildingOnMapController : MonoBehaviour
 {
-    [SerializeField] int levelTest;
-    [SerializeField] int partTest;
-    private void Update()
-    {
-        SetSkeletonAnimation(levelTest, partTest);
-    }
-    private void SetSkeletonAnimation(int level, int part)
-    {
-        var strPart = part == 0 ? "" : $"_{part}";
-        level += 0;
-        skeletonAnimation.AnimationName = $"building_lv{level}{strPart}";
-        foreach (var slotObject in slotObjects)
-        {
-            slotObject.SetActiveObject(level, part);
-        }
-    }
 }
