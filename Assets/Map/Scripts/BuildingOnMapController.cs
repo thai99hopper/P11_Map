13ad35ddfedf35030886c5a6fd5b35f2d9debbe2
Kietbm <bangminhkiet@hoppergames.io>
2025-03-12 17:46:22 +0700

    reimport spine and add saw table for building

--- Assets/Map/Scripts/BuildingOnMapController.cs
+++ Assets/Map/Scripts/BuildingOnMapController.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public partial class BuildingOnMapController : MonoBehaviour
 {
-    
+    [SerializeField] int levelTest;
+    [SerializeField] int partTest;
+    private void Update()
+    {
+        SetSkeletonAnimation(levelTest, partTest);
+    }
+    private void SetSkeletonAnimation(int level, int part)
+    {
+        var strPart = part == 0 ? "" : $"_{part}";
+        level += 0;
+        skeletonAnimation.AnimationName = $"building_lv{level}{strPart}";
+        foreach (var slotObject in slotObjects)
+        {
+            slotObject.SetActiveObject(level, part);
+        }
+    }
 }
