36195e7fb068b5dec06a1afceed88b3f7b9e4935
𝔑𝔞𝔪 𝔑𝔤𝔲𝔶𝔢𝔫 <nguyenhoainam@hoppergames.io>
2025-06-11 13:01:18 +0700

    no message

--- Assets/Map/Scripts/BuildingOnMapController.Artist.cs
+++ Assets/Map/Scripts/BuildingOnMapController.Artist.cs
@@ -1,11 +1,20 @@
-
+using System;
 using Spine.Unity;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public partial class BuildingOnMapController : MonoBehaviour
 {
+    [Serializable]
+    public class BuildingBounds
+    {
+        public float upOffset;
+        public float downOffset;
+        public float leftOffset;
+        public float rightOffset;
+        public bool debugBounds = true;
+    }
+    
     public string buildingId;
     public SkeletonAnimation skeletonAnimation;
     public GameplayButton gameplayButton;
@@ -13,57 +22,52 @@ public partial class BuildingOnMapController : MonoBehaviour
     public Transform parentPartsPos;
     public Transform parentVfxsPos;
     public List<BuildingSlotObject> slotObjects;
+    public BuildingBounds buildingBounds;
 
-    public ParticleSystem building;
-    public ParticleSystem small;
-
-
-    public List<string> nameAnims = new List<string>();
-
-    int index = 1;
-
-    public bool up1, up2;
-
-    public List<Transform> posChilds = new List<Transform>();
-
-    static string TEST = "building_lv1";
-
-    private void Start()
+    public Bounds CalculateBounds()
     {
-        var animations = skeletonAnimation.skeletonDataAsset.GetSkeletonData(true).Animations;
-        foreach (var anim in animations)
-        {
-            nameAnims.Add(anim.Name);
-        }
-
-        foreach (Transform child in parentPartsPos)
-        {
-            posChilds.Add(child);
-        }
-
-        skeletonAnimation.AnimationName = nameAnims[index];
+        var leftUp = transform.position + new Vector3(-buildingBounds.leftOffset, buildingBounds.upOffset, 0);
+        var rightBottom = transform.position + new Vector3(buildingBounds.rightOffset, -buildingBounds.downOffset, 0);
+        var center = (leftUp + rightBottom) / 2f;
+        var sz = new Vector2(Mathf.Abs(leftUp.x - rightBottom.x), Mathf.Abs(leftUp.y - rightBottom.y));
+        return new Bounds(center, sz);
     }
 
-    private void Update()
-    {
-        if (up1 != up2 && index < nameAnims.Count)
-        {
-            index++;
-            skeletonAnimation.AnimationName = nameAnims[index];
-            up1 = !up1;
+    #region gizmos
 
-            if (TEST.Length < nameAnims[index].Length)
-            {
-                if (small == null) Debug.LogError("reference the object into building when want to test");
-                var transo = Instantiate(small, posChilds[index]);
-                transo.transform.localPosition = Vector3.zero;
-            }
-            else
-            {
-                if (building == null) Debug.LogError("reference the object into building when want to test");
-                var transo = Instantiate(building, posChilds[index]);
-                transo.transform.localPosition = Vector3.zero;
-            }
+    private void OnDrawGizmosSelected()
+    {
+        if (!buildingBounds.debugBounds)
+        {
+            return;
         }
+
+        DrawBounds(CalculateBounds());
     }
+    
+    private static void DrawBounds(Bounds bounds)
+    {
+        var min = bounds.min;
+        var max = bounds.max;
+        var p1 = new Vector2(min.x, min.y);
+        var p2 = new Vector2(max.x, min.y);
+        var p3 = new Vector2(max.x, max.y);
+        var p4 = new Vector2(min.x, max.y);
+        DrawLine(p1, p2);
+        DrawLine(p2, p3);
+        DrawLine(p3, p4);
+        DrawLine(p4, p1);
+    }
+    
+    private static void DrawLine(Vector2 p1, Vector2 p2)
+    {
+        var color = Gizmos.color;
+        Gizmos.color = Color.red;
+        
+        Gizmos.DrawLine(p1, p2);
+        
+        Gizmos.color = color;
+    }
+    
+    #endregion
 }
